be1b6b6320856f93955d8548eecc7ed930ae1136
vctrdavidsom <vctrdavidsomdev@gmail.com>
2023-10-23 17:21:08 -0300

    segundo commit

--- poker.js
+++ poker.js
@@ -1,6 +1,7 @@
 let currentPhase = "Pré-Flop";
         let currentPlayer = 1;
         setCurrentPlayer(currentPlayer);
+        let newRaise = 0;
 
         let pote = 0;
         const players = {};
@@ -37,7 +38,7 @@ let currentPhase = "Pré-Flop";
         }
 
         function call(playerNumber) {
-            const raiseAmount = pote;
+            const raiseAmount = newRaise;
             if (players[`player${playerNumber}`].chips >= raiseAmount) {
                 players[`player${playerNumber}`].chips -= raiseAmount;
                 pote += raiseAmount;
@@ -54,6 +55,7 @@ let currentPhase = "Pré-Flop";
             const raiseAmount = parseInt(prompt(`Quanto ${players[`player${playerNumber}`].name} deseja apostar no Raise?`));
             if (!isNaN(raiseAmount) && raiseAmount >= 0 && raiseAmount <= players[`player${playerNumber}`].chips) {
                 players[`player${playerNumber}`].chips -= raiseAmount;
+                newRaise = raiseAmount;
                 pote += raiseAmount;
                 document.getElementById("pote").textContent = `Pote: ${pote}`;
                 document.getElementById(`player${playerNumber}-info`).innerHTML = `<div class="player-info">${players[`player${playerNumber}`].name}</div>Fichas: ${players[`player${playerNumber}`].chips}`;
@@ -67,20 +69,44 @@ let currentPhase = "Pré-Flop";
         function nextPhase() {
             if (currentPhase === "Pré-Flop") {
                 currentPhase = "Flop";
+                newRaise = 0;
             } else if (currentPhase === "Flop") {
                 currentPhase = "Turn";
+                newRaise = 0;
             } else if (currentPhase === "Turn") {
                 currentPhase = "River";
+                newRaise = 0;
             } else if (currentPhase === "River") {
                 currentPhase = "Showdown";
+                newRaise = 0;
             } else if (currentPhase === "Showdown") {
                 currentPhase = "Pré-Flop"; // Recomeçar o jogo
                 currentPlayer = 1;
+                newRaise = 0;
                 setCurrentPlayer(currentPlayer);
             }
             document.getElementById("game-phase").textContent = `Fase: ${currentPhase}`;
         }
         
         function setCurrentPlayer(currentPlayer){
-            document.getElementById("currentPlayer").innerHTML = `Jogador atual: ${currentPlayer}`;
+            document.getElementById("currentPlayer").innerHTML = `Ação do jogador: ${currentPlayer}`;
         }
+
+        //_________________________________________________________________________________________________
+       
+        function endGame() {
+            const vencedorNome = prompt("Informe o nome do jogador vencedor:");
+            for (const playerKey in players) {
+                if (players[playerKey].name === vencedorNome) {
+                    const vencedor = playerKey;
+                    alert(`${vencedor} venceu a partida! Pote: ${pote}`);
+                    players[vencedor].chips += pote;
+                    document.getElementById("pote").textContent = `Pote: ${pote}`;
+                    nextPlayer(); // Avança para o próximo jogador
+                    setCurrentPlayer(currentPlayer); // Atualiza o jogador atual
+                    return;
+                }
+            }
+            alert("Jogador não encontrado ou já está fora da partida. Tente novamente.");
+        }
+        
