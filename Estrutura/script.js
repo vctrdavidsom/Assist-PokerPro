f03e1478733af43d8694de71ae11f3db60fa64b1
Luan Ferreira <85955653+lhferreirad@users.noreply.github.com>
2023-11-25 22:09:29 -0300

    Update script.js
    cruzeiro querido

--- Estrutura/script.js
+++ Estrutura/script.js
@@ -1,152 +1,116 @@
-body, div, h1, h2, h3, p, figure {
-    margin: 0;
-    padding: 0;
-    border: 0;
-}
+let currentPhase = "Pré-Flop";
+        let currentPlayer = 1;
+        setCurrentPlayer(currentPlayer);
+        let newRaise = 0;
 
-body {
-    background-image: url(../img/BackgroundBory.jpg);
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center center;
-}
+        let pote = 0;
+        const players = {};
 
-#game {
-    background-image: url(../img/BackgroundDaDIv.png);
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center center;
-    height: 490px;
-    width: 1250px;
-    flex-direction: row;
-    padding: -2%;
-    margin: 2%;
-    align-items: center;
-    position: relative;
-    left: 120px;
-    border-radius: 80px;
-}
-h1{
-    text-align: center;
-    color:  #fed38e;
-}
-h2{
-    position: relative;
-    color: #ff0000;
-    top: 80px;
-    left: 540px;
-}
-/*.action-button{
-    width: 40px;
-    height: 15px;
-    font-size: 10px;
-}
-.action-button-player{
-    width: 150px;
-    height: 15px;
-    font-size: 10px;
-}*/
+        function addPlayer(playerNumber) {
+            const playerName = prompt("Nome do jogador:");
+            const chips = parseInt(prompt("Quantidade de fichas:"));
+            if (playerName && !isNaN(chips) && chips >= 0) {
+                players[`player${playerNumber}`] = { name: playerName, chips: chips };
+                document.getElementById(`player${playerNumber}-info`).innerHTML = `<div class="player-info">${playerName}</div>Fichas: ${chips}`;
+            }
+        }
 
-#pote {
-    position: relative;
-    color: #ff0000;
-    bottom: 150px;
-    left: 138px;
-    font-size: 50px;
+        function nextPlayer() {
+            if (currentPlayer === 6 || currentPlayer == Object.keys(players).length) {
+                currentPlayer = 1;
+            } else {
+                currentPlayer++;
+            }
+            setCurrentPlayer(currentPlayer);
 
-}
-.player-slot{
-    color: #ccc;
-}
-#game {
-    position: relative;
-    display: grid;
-    padding-bottom: 65px;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
-}
+        }
 
-#player1 {
-    width: 240px;
-    height: 50px;
-    position: relative;
-    left: -200px;
-    top: -35px;
-    bottom: 7px;
-    border-radius: 80px;
-}
+        function fold(playerNumber) {
+            alert(`${players[`player${playerNumber}`].name} escolheu Fold.`);
+            
+            nextPlayer();
+        }
 
-#player2 {
-    width: 240px;
-    height: 50px;
-    position: relative;
-    top: -35px;
-    bottom: 7px;
-    right: 25px;
-    border-radius: 80px;
-}
+        function check(playerNumber) {
+            alert(`${players[`player${playerNumber}`].name} escolheu Check.`);
+            nextPlayer();
+        }
 
-#player3 {
-    width: 210px;
-    height: 80px;
-    position: relative;
-    left: 1005px;
-    top: 30px;
-    border-radius: 80px;
-}
+        function call(playerNumber) {
+            const raiseAmount = newRaise;
+            if (players[`player${playerNumber}`].chips >= raiseAmount) {
+                players[`player${playerNumber}`].chips -= raiseAmount;
+                pote += raiseAmount;
+                document.getElementById("pote").textContent = `Pote: ${pote}`;
+                document.getElementById(`player${playerNumber}-info`).innerHTML = `<div class="player-info">${players[`player${playerNumber}`].name}</div>Fichas: ${players[`player${playerNumber}`].chips}`;
+                alert(`${players[`player${playerNumber}`].name} escolheu Call.`);
+            } else {
+                alert(`${players[`player${playerNumber}`].name} não possui fichas suficientes para dar Call.`);
+            }
+            nextPlayer();
+        }
 
-#player4 {
-    width: 240px;
-    height: 50px;
-    position: relative;
-    left: 360px;
-    top: 285px;
-    border-radius: 80px;
-}
+        function raise(playerNumber) {
+            const raiseAmount = parseInt(prompt(`Quanto ${players[`player${playerNumber}`].name} deseja apostar no Raise?`));
+            if (!isNaN(raiseAmount) && raiseAmount >= 0 && raiseAmount <= players[`player${playerNumber}`].chips) {
+                players[`player${playerNumber}`].chips -= raiseAmount;
+                newRaise = raiseAmount;
+                pote += raiseAmount;
+                document.getElementById("pote").textContent = `Pote: ${pote}`;
+                document.getElementById(`player${playerNumber}-info`).innerHTML = `<div class="player-info">${players[`player${playerNumber}`].name}</div>Fichas: ${players[`player${playerNumber}`].chips}`;
+                alert(`${players[`player${playerNumber}`].name} escolheu Raise ${raiseAmount}.`);
+            } else {
+                alert("Valor de Raise inválido.");
+            }
+            nextPlayer();
+        }
 
-#player5 {
-    width: 240px;
-    height: 50px;
-    position: relative;
-    right: 585px;
-    top: 240px;
-    top: 285px;
-    border-radius: 80px;
-}
+        function nextPhase() { // passa para proxima fase e zera a variavel que armazarna a aposta da rodada
+            if (currentPhase === "Pré-Flop") {
+                currentPhase = "Flop";
+                newRaise = 0;
+            } else if (currentPhase === "Flop") {
+                currentPhase = "Turn";
+                newRaise = 0;
+            } else if (currentPhase === "Turn") {
+                currentPhase = "River";
+                newRaise = 0;
+            } else if (currentPhase === "River") {
+                currentPhase = "Showdown";
+                newRaise = 0;
+            } else if (currentPhase === "Showdown") {
+                currentPhase = "Pré-Flop"; // Recomeçar o jogo
+                currentPlayer = 1;
+                newRaise = 0;
+                setCurrentPlayer(currentPlayer);
+            }
+            document.getElementById("game-phase").textContent = `Fase: ${currentPhase}`;
+        }
+        
+        function setCurrentPlayer(currentPlayer){
+            document.getElementById("currentPlayer").innerHTML = `Ação do jogador: ${currentPlayer}`;
+        }
 
-#player6 {
-    width: 210px;
-    height: 80px;
-    position: relative;
-    bottom: 140px;
-    left: 5px;
-    border-radius: 80px;
+        //_________________________________________________________________________________________________
+       
+        function endGame() {// finaliza a partida declara o vencedor
+            const vencedorNome = prompt("Informe o nome do jogador vencedor:");
+            for (const playerKey in players) {
+                if (players[playerKey].name === vencedorNome) {
+                    const vencedor = playerKey;
+                    alert(`${vencedor} venceu a partida! Pote: ${pote}`);
+                    players[vencedor].chips += pote;
+                    pote = 0
+                    currentPhase = "Pré-Flop"
+                    document.getElementById("pote").textContent = `Pote: ${pote}`;
+                    document.getElementById(`${playerKey}-info`).innerHTML = `<div class="player-info">${players[`${playerKey}`].name}</div>Fichas: ${players[`${playerKey}`].chips}`;
+                    document.getElementById("game-phase").textContent = `Fase: ${currentPhase}`;
 
-}
-
-.player-slot {
-    padding: 10px;
-    border: 1px solid #ccc;
-    text-align: center;
-}
-.player-info {
-    font-weight: bold;
-}
-#game-phase {
-    color: #ff0000;
-    text-align: center;
-    font-size: 20px;
-    margin-top: 20px;
-    position: relative;
-    bottom: 5px;
-}
-#next-phase-button {
-    text-align: center;
-    margin-top: 10px;
-    position: relative;
-    bottom: 35px;
-    right: 420px;
-}
-.action-button {
-    margin: 5px;
-}
+                    nextPlayer(); // Avança para o próximo jogador
+                    setCurrentPlayer(currentPlayer); // Atualiza o jogador atual
+                    return;
+                }
+            }
+            alert("Jogador não encontrado ou já está fora da partida. Tente novamente.");
+        }
+        
